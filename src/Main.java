b5621951cd8d0e9357a4d9cd17b661511fe890b1
Ben Morris-Rains <benmorrisrains@gmail.com>
2018-01-02 21:44:14 -0700

    Added ability to shuffle one out faro and track card

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import Tracker.NewDeckOrder;
+import Tracker.OutFaro;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -26,12 +27,27 @@ public class Main {
         if(temp.contains(cardToTrack)) {
             cardsPosition += temp.indexOf(cardToTrack) + 1;
         }
-        System.out.println("\n" + cardToTrack + " found at position " + cardsPosition + ".");
+        System.out.println("\n" + cardToTrack + " starting position is " + cardsPosition + ".");
+        System.out.println("\nPerforming one out faro.");
 
+        OutFaro outFaro = new OutFaro();
 
+        String[] firstHalfSuites = new String[]{"Hearts", "Clubs"};
+        String[] firstHalfValues = new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
+        String[] secondHalfSuites = new String[]{"Diamonds", "Spades"};
+        String[] secondHalfValues = new String[]{"King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2", "Ace"};
 
+        System.out.println("First shuffle results: " + outFaro.OutFaro(firstHalfSuites, firstHalfValues, secondHalfSuites, secondHalfValues));
 
+        int cardAfterOneShuffle = 0;
+        if (outFaro.OutFaro(firstHalfSuites, firstHalfValues,
+                secondHalfSuites, secondHalfValues).contains(cardToTrack)) {
+            cardAfterOneShuffle += outFaro.OutFaro(firstHalfSuites, firstHalfValues,
+                    secondHalfSuites, secondHalfValues).indexOf(cardToTrack) + 1;
+
+        }
+        System.out.println("After one shuffle, your card is at number " + cardAfterOneShuffle);
 
     }
 
--- src/Tracker/OutFaro.java
+++ src/Tracker/OutFaro.java
@@ -1,16 +1,62 @@
 package Tracker;
 
-public class OutFaro extends NewDeckOrder {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-    public String cardToTrack;
-    NewDeckOrder deckOrder;
+public class OutFaro {
+    ArrayList<String> first26;
+    ArrayList<String> last26;
+    ArrayList<String> oneShuffle;
+    String[] suitesFirstHalf;
+    String[] valuesFirstHalf;
+    String[] suitesSecondHalf;
+    String[] valuesSecondHalf;
 
-    // locate card
+
+    public ArrayList<String> OutFaro(String[] suitesFirstHalf, String[] valuesFirstHalf,
+                                     String[] suitesSecondHalf, String[] valuesSecondHalf) {
+
+        this.suitesFirstHalf = suitesFirstHalf;
+        this.valuesFirstHalf = valuesFirstHalf;
+        this.suitesSecondHalf = suitesSecondHalf;
+        this.valuesSecondHalf = valuesSecondHalf;
 
 
+        first26 = new ArrayList<>();
 
-    //pick amount of out faro shuffles
+        for (String deckFirstSuites : suitesFirstHalf) {
+            for (String deckFirstValues : valuesFirstHalf) {
+                first26.add(deckFirstValues + " of " + deckFirstSuites);
+            }
+        }
 
-    // show numeric value of chosen card after shuffles
+        last26 = new ArrayList<>();
 
+        for (String deckSecondSuites : suitesSecondHalf) {
+            for (String deckSecondValues : valuesSecondHalf) {
+                last26.add(deckSecondValues + " of " + deckSecondSuites);
+            }
+        }
+
+        // One Shuffle
+        oneShuffle = new ArrayList<>();
+
+        for (int i = 0; i < first26.size() - 1; i++) {
+            for (int j = 0; j < last26.size() - 1; j++) {
+                oneShuffle.add(first26.get(i));
+                oneShuffle.add(last26.get(j));
+                i++;
+
+            }
+        }
+
+        return oneShuffle;
+    }
+
+    @Override
+    public String toString() {
+        return "OutFaro{" +
+                "oneShuffle=" + oneShuffle +
+                '}';
+    }
 }
